530a06160eb0a03756c41d0c96ddca3bfe423887
Reuven Yanturin <reuven.yanturin@gmail.com>
2024-12-19 21:32:15 +0200

    Updates

--- assets/DashBoard-tLblGy-J.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as r,o as a}from"./index-MNRb34Qu.js";import{V as s}from"./VContainer-CidzjlCO.js";const o={};function c(n,t){return a(),r(s)}const f=e(o,[["render",c],["__file","/Users/reuvenyanturin/Projects/WiseWallet/admin/src/pages/DashBoard.vue"]]);export{f as default};
